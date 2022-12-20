cc0fa6c2de56c7ea8085facba423d88b044a4ab0
Nadia Zubko <ne.nadezda@gmail.com>
2022-12-20 11:13:54 -0700

    Update content

--- src/components/HeaderHome.js
+++ src/components/HeaderHome.js
@@ -2,7 +2,7 @@ import cafe from '../assets/cafe-table-bun.png'
 
 const HeaderHome = () => {
     return (
-        <header className="header container">
+        <header className="header container" id="home">
             <div className="row justify-content-md-between">
                 <div class="col-md-6 d-flex flex-column align-self-center ">
                     <h2 className="visually-hidden">Header</h2>
@@ -10,7 +10,7 @@ const HeaderHome = () => {
                         We serve a selection of coffee and cocktails
                     </h3>
                     <span className="mt-2">
-                        Coffee Expresso Yourself is a warm and welcoming space
+                        Coffee Espresso Yourself is a warm and welcoming space
                         where you can enjoy a wide range of drinks and food.
                         Whether you're looking for freshly brewed coffee and
                         pastries to craft beers and cocktails, as well as small
@@ -23,9 +23,11 @@ const HeaderHome = () => {
                         <h3 className="mb-2">Opening Times</h3>
                         <span>Sunday to Saturday | 09:00 AM to 11:00 PM</span>
                         <h3 className="mt-3 mb-2">Location</h3>
-                        <span>Master canteen, BBSR , Odisha 752054</span>
+                        <span>
+                            Master canteen, 4212 Feathers Hooves Drive, NY 11717
+                        </span>
                         <h3 className="mt-3 mb-2">Call us</h3>
-                        <span>+9776462441</span>
+                        <span>(631) 625-6181</span>
                     </section>
                 </div>
                 <div class="col-md-6">
